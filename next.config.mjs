247609a808676ab257a0ad821dc205a041ff4962
Jackson <dickfosjackson@gmail.com>
2025-11-05 23:50:17 +0100

    Add explicit pageExtensions to Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 import createMDX from "@next/mdx";
 
-/** @type {import(next).NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: { mdxRs: true },
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 };
 
 const withMDX = createMDX({
