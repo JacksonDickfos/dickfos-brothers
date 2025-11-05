ac94eca3ccbb1e72262287988a16ae2e7afb5fdb
Jackson <dickfosjackson@gmail.com>
2025-11-06 00:03:51 +0100

    Add not-found page and ensure Next.js config is optimized for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { mdxRs: true },
+  experimental: { 
+    mdxRs: true,
+  },
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 };
 
